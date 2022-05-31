88b686784d9981aa695eb9b15325a1a82549346a
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-05-31 16:12:41 +0000

    cleaned up the code a bit

--- Main.hs
+++ Main.hs
@@ -4,15 +4,6 @@
 -- --  State s -> (a,s)
 -- --  reader r -> a
 
--- type Value = Int
-
--- data Expr = Lit Value
---           | Add Expr Expr
---           | Mul Expr Expr
---           deriving (Show)
-
-newtype ExceptT e m a = ExceptT (m (Either e a))
-
 bindExceptT :: Monad m => m (Either e a) -> (a -> m (Either e b)) -> m (Either e b)
 bindExceptT mx f = do
   x <- mx -- `x` has the type `Either e a`
