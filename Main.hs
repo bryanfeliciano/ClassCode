ce02dbd6a1599a10bc05a910bb1a100b97f472da
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-06-03 22:45:11 +0000

    practicing some new concepts learned in class

--- Main.hs
+++ Main.hs
@@ -6,7 +6,15 @@
 
 bindExceptT :: Monad m => m (Either e a) -> (a -> m (Either e b)) -> m (Either e b)
 bindExceptT mx f = do
-  x <- mx -- `x` has the type `Either e a`
+  x <- mx -- `x` has the type `E e a`
   case x of
     Left err -> pure (Left err)
     Right y -> f y
+
+-- Implementation of state and Io together
+
+type World s m a  = s -> m  (a,s) 
+type StateIO s a = s -> IO (s,a)
+
+-- return for a custom state would look something like this 
+-- returnStateIO :: a -> (State s) a 
